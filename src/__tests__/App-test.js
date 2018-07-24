4a596305d5aa3445c63a210baedb1578681efcae
Phil Stainer <philstainer.dev@gmail.com>
2018-07-24 19:54:20 +0100

    Add render test for app component

---
+++ src/__tests__/App-test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+
+import App from '../App';
+import { findByTestAttr } from '../../helpers/testUtils'
+
+const setup = () => {
+  const wrapper = shallow(<App />)
+  return wrapper
+}
+
+describe('App', () => {
+  it('should render without crashing', () => {
+    const wrapper = setup()
+    const component = findByTestAttr(wrapper, 'component-app')
+    expect(component.length).toBe(1)
+  })
+})
