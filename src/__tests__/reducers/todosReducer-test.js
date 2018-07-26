6d9019f99545fb70fbdcdae994c4adae4f220ebe
Phil Stainer <philstainer.dev@gmail.com>
2018-07-26 19:53:56 +0100

    Add `todoReducer` tests and code

---
+++ src/__tests__/reducers/todosReducer-test.js
@@ -0,0 +1,63 @@
+import todosReducer from '../../reducers/todosReducer'
+
+describe('todos reducer', () => {
+
+  it('should handle initial state', () => {
+    expect(todosReducer(undefined, {})).toEqual([])
+  })
+
+  it('should handle ADD_TODO', () => {
+    expect(
+      todosReducer([], {
+        type: 'ADD_TODO',
+        text: 'First todo',
+        id: 0
+      })
+    ).toEqual([{
+      text: 'First todo',
+      completed: false,
+      id: 0
+    }])
+
+    expect(
+      todosReducer([
+        {
+          text: 'First todo',
+          completed: false,
+          id: 0
+        }
+      ], {
+          type: 'ADD_TODO',
+          text: 'Second todo',
+          id: 1
+        })
+
+    ).toEqual([
+      {
+        text: 'First todo',
+        completed: false,
+        id: 0
+      }, {
+        text: 'Second todo',
+        completed: false,
+        id: 1
+      }])
+  })
+
+  it('should handle TOGGLE_TODO', () => {
+    expect(
+      todosReducer([{
+        text: 'First todo',
+        completed: false,
+        id: 1
+      }], {
+          type: 'TOGGLE_TODO',
+          id: 1
+        })
+    ).toEqual([{
+      text: 'First todo',
+      completed: true,
+      id: 1
+    }])
+  })
+})
--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,5 +1,12 @@
+import ACTION_TYPES from '../actions'
+
 export default (state = [], action) => {
   switch (action.type) {
+    case ACTION_TYPES.ADD_TODO:
+      return [...state, { text: action.text, completed: false, id: action.id }]
+
+    case ACTION_TYPES.TOGGLE_TODO:
+      return state.map(todo => (todo.id === action.id) ? { ...todo, completed: !todo.completed } : todo)
 
     default:
       return state
