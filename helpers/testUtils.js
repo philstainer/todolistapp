a3dfbbf725b0afb336a8067bff3bc1c586ec902f
Phil Stainer <philstainer.dev@gmail.com>
2018-08-05 18:15:29 +0100

    Add `storeFactory` test util

--- helpers/testUtils.js
+++ helpers/testUtils.js
@@ -1,5 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import checkPropTypes from 'check-prop-types';
+import { createStore, applyMiddleware } from 'redux';
+
+import rootReducer from '../src/reducers';
+import { middlewares } from '../src/store/configureStore';
 
 export const findByTestAttr = (wrapper, attribute) => (
   wrapper.find(`[data-test="${attribute}"]`)
@@ -9,4 +13,9 @@ export const checkProps = (component, conformingProps) => {
   // eslint-disable-next-line react/forbid-foreign-prop-types
   const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name);
   expect(propError).toBeUndefined();
+};
+
+export const storeFactory = (initialState) => {
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 
-import rootReducer from './reducers';
+import rootReducer from '../reducers';
 
 export const middlewares = [ReduxThunk];
 
