2b2052243ba3d59e02f43e378f90de6ab6783272
Phil Stainer <philstainer.dev@gmail.com>
2018-08-05 14:40:18 +0100

    Add ESlint Ignore rules `testUtils`

--- helpers/testUtils.js
+++ helpers/testUtils.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import checkPropTypes from 'check-prop-types';
 
 export const findByTestAttr = (wrapper, attribute) => (
@@ -5,6 +6,7 @@ export const findByTestAttr = (wrapper, attribute) => (
 );
 
 export const checkProps = (component, conformingProps) => {
+  // eslint-disable-next-line react/forbid-foreign-prop-types
   const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name);
   expect(propError).toBeUndefined();
 };
